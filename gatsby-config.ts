976619eeb1c214b11a0a1d8448f99f252236fd0f
halfcyan <nicholasreed04@gmail.com>
2022-10-02 18:00:10 -0500

    it's an actual website now

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,26 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://cinderithink.lol`,
+    title: `cinder's website`,
+    author: {
+      name: `cinder`,
+      summary: `big gay like massive gay.`,
+    },
+    description: `I don't even know to be honest`,
+    social: {
+      twitter: `cinderithink`,
+    },
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 }
 
