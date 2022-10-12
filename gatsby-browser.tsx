f072b29dbeab4403daa834ead6968760a065c2e2
halfcyan <nicholasreed04@gmail.com>
2022-10-12 17:34:48 -0500

    Update

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,1 +1,16 @@
 import './src/styles/global.css'
+
+import { createFilePath } from 'gatsby-source-filesystem'
+
+// Convert to ES6
+export const onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === 'MarkdownRemark') {
+      const slug = createFilePath({ node, getNode, basePath: 'pages' })
+      createNodeField({
+        node,
+        name: 'slug',
+        value: slug,
+      })
+    }
+  }
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,6 +22,14 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
 
